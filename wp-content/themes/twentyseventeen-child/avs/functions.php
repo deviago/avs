247902d98b47da066ffafac5e72d1415e71e2cd0
vijay <vijay.carpenter@techinfini.com>
2018-11-06 13:38:03 +0530

    Resolving issue with changes

--- wp-content/themes/twentyseventeen-child/avs/functions.php
+++ wp-content/themes/twentyseventeen-child/avs/functions.php
@@ -10,6 +10,8 @@ function avs_enqueue_styles_n_scripts() {
   wp_enqueue_script( 'avs-iframtransportjs', get_theme_file_uri('avs/assets/js/jquery.iframe-transport.js'), array('jquery'), '1.0', false );
   wp_enqueue_script( 'avs-fileuploadjs', get_theme_file_uri('avs/assets/js/jquery.fileupload.js'), array('jquery'), '1.0', false );
   wp_enqueue_script( 'avs-fusejs', get_theme_file_uri('avs/assets/js/fuse.min.js'), array('jquery'), '1.0', false );
+
+  wp_enqueue_script( 'avs-b-popup', get_theme_file_uri('avs/assets/js/jquery.bpopup.min.js'), array('jquery'), '1.0', false );
 }
 add_action( 'wp_enqueue_scripts', 'avs_enqueue_styles_n_scripts' );
 
@@ -43,9 +45,10 @@ function avs_get_google_sheet_data() {
   $client = new Google_Client();
   $client->setAuthConfig(WP_THEME_PATH.'/avs/google-api-php-client/client_secrets.json');
   $client->addScope(Google_Service_Sheets::SPREADSHEETS_READONLY);
+  $client->addScope('https://www.googleapis.com/auth/drive');
 
   $response = ['error' => false];
-  $tokenPath = WP_THEME_PATH.'/avs/google-api-php-client/token.json';
+  $tokenPath = WP_THEME_PATH.'/avs/google-api-php-client/token-'.get_current_user_id().'.json';
 
   if ( file_exists($tokenPath) ) { // Check if token exist
     $accessToken = json_decode(file_get_contents($tokenPath), true);
@@ -57,16 +60,48 @@ function avs_get_google_sheet_data() {
       exit();
     }else{
       $service = new Google_Service_Sheets($client);
-      $spreadsheetId = '1K02_5cn9TtjhybnBQYYVRmzjl0sYPnIu_Ry0PSEKsOk'; // https://docs.google.com/spreadsheets/d/1K02_5cn9TtjhybnBQYYVRmzjl0sYPnIu_Ry0PSEKsOk
+
+      $user = wp_get_current_user();
+      $roles = (array) $user->roles;
+
+      $tmpListings = $listings = array();
       $range = 'Orders!A2:R';
-      $gResponse = $service->spreadsheets_values->get($spreadsheetId, $range);
-      $response['listings'] = json_encode($gResponse->getValues());
+
+      if( in_array('shop_manager', $roles) || in_array('administrator', $roles) ){
+				if( isset($_REQUEST['writer']) ) {
+					$spreadsheetId = get_user_meta($_REQUEST['writer'], 'avs_googlesheet_id', true);
+	        if($spreadsheetId){
+	          $gResponse = $service->spreadsheets_values->get($spreadsheetId, $range);
+	          $tmpListings[] = $gResponse->getValues();
+	        }
+				}else{
+					$avs_writers = get_users('role=avs_writer');
+	        foreach ( $avs_writers as $avs_writer ) {
+	          $spreadsheetId = get_user_meta($avs_writer->ID, 'avs_googlesheet_id', true);
+	          if($spreadsheetId){
+	            $gResponse = $service->spreadsheets_values->get($spreadsheetId, $range);
+	            $tmpListings[] = $gResponse->getValues();
+	          }
+	        }	
+				}
+      }else{
+        $spreadsheetId = get_user_meta(get_current_user_id(), 'avs_googlesheet_id', true);
+        if($spreadsheetId){
+          $gResponse = $service->spreadsheets_values->get($spreadsheetId, $range);
+          $tmpListings[] = $gResponse->getValues();
+        }
+      }
+
+      foreach($tmpListings as $key => $value){
+        $listings = array_merge($listings, $value);
+      }
+
+      $response['listings'] = json_encode($listings);
     }
   } else {
     header('Location: ' . filter_var($client->createAuthUrl(), FILTER_SANITIZE_URL));
     exit();
   }
-
   return $response;
 }
 
@@ -76,12 +111,13 @@ function avs_get_google_sheet_data() {
 function avs_update_google_sheet_access_token(){
   require_once WP_THEME_PATH.'/avs/google-api-php-client/vendor/autoload.php';
   
-  $tokenPath = WP_THEME_PATH.'/avs/google-api-php-client/token.json';
+  $tokenPath = WP_THEME_PATH.'/avs/google-api-php-client/token-'.get_current_user_id().'.json';
 
   $client = new Google_Client();
   $client->setAuthConfigFile(WP_THEME_PATH.'/avs/google-api-php-client/client_secrets.json');
   $client->setRedirectUri(WP_SITE_URL.'/wp-admin/admin-ajax.php?action=avs_google_sheet_access_token');
   $client->addScope(Google_Service_Sheets::SPREADSHEETS_READONLY);
+  $client->addScope('https://www.googleapis.com/auth/drive');
 
   if( isset($_GET['code']) ) {
     $client->authenticate($_GET['code']);
@@ -94,23 +130,23 @@ function avs_update_google_sheet_access_token(){
     $auth_url = $client->createAuthUrl();
     header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
   }
-  exit();
+  
 }
 add_action( 'wp_ajax_avs_google_sheet_access_token', 'avs_update_google_sheet_access_token' );
 
 /**
- * Process Google Drive document upload
+ * Process document for validation
  */
-function avs_upload_document_to_google_drive() {
+function avs_save_file_to_g_drive() {
   require_once WP_THEME_PATH.'/avs/google-api-php-client/vendor/autoload.php';
 
   $client = new Google_Client();
   $client->setAuthConfig(WP_THEME_PATH.'/avs/google-api-php-client/client_secrets.json');
   $client->addScope(Google_Service_Sheets::SPREADSHEETS_READONLY);
-  $client->addScope('https://www.googleapis.com/auth/drive');  
+  $client->addScope('https://www.googleapis.com/auth/drive');
 
   $response = ['error' => false];
-  $tokenPath = WP_THEME_PATH.'/avs/google-api-php-client/token.json';
+  $tokenPath = WP_THEME_PATH.'/avs/google-api-php-client/token-'.get_current_user_id().'.json';
 
   if ( file_exists($tokenPath) ) { // Check if token exist
     $accessToken = json_decode(file_get_contents($tokenPath), true);
@@ -131,31 +167,48 @@ function avs_upload_document_to_google_drive() {
         'fields' => 'id'
       ));
 
-      printf('File ID: %s\n', $file->id);
+      printf("File ID: %s\n", $file->id);
     }
   } else {
     header('Location: ' . filter_var($client->createAuthUrl(), FILTER_SANITIZE_URL));
     exit();
   }
 }
-add_action( 'wp_ajax_avs_upload_document_to_google_drive', 'avs_upload_document_to_google_drive' );
+add_action( 'wp_ajax_avs_save_file_to_g_drive', 'avs_save_file_to_g_drive' );
 
 /**
- * Process document for validation
+ * Process file validation
  */
 function avs_process_document(){
-  if( isset($_REQUEST['file_name']) ){
-    $file_name = $_REQUEST['file_name'];
-    $file_path = WP_UPLOAD_DIR.'/articles-docs/files/'.$file_name;
-    
-    $images = avs_doc_image_count($file_path);
-    $title = avs_doc_file_title_opertion($file_name, $file_path);
-    $length = avs_read_doc_length($file_path);
 
-    $results = array_merge($images, $title, $length);
+  if( isset($_REQUEST['file_name']) && isset($_REQUEST['article_data']) ){
+    $file_name = $_REQUEST['file_name'];
+    $article_data = json_decode(stripslashes($_REQUEST['article_data']));
+    $file_path = WP_UPLOAD_DIR.'/server/files/'.$file_name;
+
+    /* avs_save_file_to_g_drive($file_name, $file_path); */
+    $images = avs_doc_image_count($file_path, $article_data);
+    $title = avs_doc_file_title_opertion($file_name, $file_path, $article_data);
+    $length = avs_read_doc_length($file_path, $article_data);
+/*
+        echo $images;
+    print_r($title);
+    echo $length;
+    die;*/
+    //print_r($images);die;
+    $results =array();
+    if($images){ $results[] = $images; }
+    if($length){ $results[] = $length; }
+    
+    $results = array_merge( $title,$results);
+   
+     if(empty($results)){
+      $results = null;
+     }
+
     echo json_encode($results);
-  }
-  exit();
+    exit(); 
+  } 
 }
 add_action( 'wp_ajax_avs_process_document', 'avs_process_document' );
 
@@ -164,10 +217,10 @@ add_action( 'wp_ajax_avs_process_document', 'avs_process_document' );
  * @param string $file_path File path
  * @return array
  */
-function avs_doc_image_count($file_path) {
+function avs_doc_image_count($file_path, $article_data) {
+  $response = array();
   $zip = new ZipArchive;
   $flag = 0; // set flag to get count of image
-  $response = array();
 
   if (true === $zip->open($file_path)) { // Open the received archive file
     for ($i=0; $i<$zip->numFiles;$i++) {
@@ -178,13 +231,11 @@ function avs_doc_image_count($file_path) {
     }
   }
 
-  if ($flag <= $zip->numFiles) {
-    $response['images'] = 'Number of images: '.$flag;
-  }else{
-    $response['images'] = 'The number of images in the .doc doesn’t match the column';
+  //echo $flag;echo $article_data[13];die;
+  if ($flag < $article_data[13]) {
+    return 'Expected number of Images:'.$article_data[13].' Number of images in article: '.$flag;
   }
-
-  return $response;
+  return false;
 }
 
 /*
@@ -193,36 +244,69 @@ function avs_doc_image_count($file_path) {
  * @param string $file_path File path
  * @return array
  */
-function avs_doc_file_title_opertion($filename, $file_path){
+function avs_doc_file_title_opertion($file_name, $file_path, $article_data){
   $response = array();
-    
-  $file_name = explode('.', $filename);
-  $file_title = explode(' - ', $file_name[0]);
-  $splitdata = str_split($file_name[0],12);
-    
-  $response['title'] = 'File Title: '.$file_title[1];
-  $response['uid'] = 'UID: '.$splitdata[0];
-  
-  if (strlen($file_name[0] > 55)) { // get error when len of char more then 55
-    $response['title_length_exceed'] = 'The title should be less than 55 characters';
-    $response['title_length'] = 'Article length is: '.strlen($file_name[0]);
-  }
+  $file_name_ext = explode('.', $file_name);
+  $file_title = explode(' - ', $file_name_ext[0]);
+  $splitdata = str_split($file_name_ext[0],11);
+  $length = strlen($file_title[0]);
  
-  $UID =$splitdata[0];  // check UID is exist of not in file name
-  if( strpos( $file_name, $UID ) == false) {
-    $response['uid_not_present'] = 'UID is not exist in file name';
-  }
-
+  if ($length < 55) {  //$file_title[1]
+   // do nothing
+  } else {
+      $response[] = 'Max title length: 55 characters. Article title length: '.strlen($file_title[0]); 
+    }
+    
+  // check UID is exist of not in file name
+ /* if (strpos($file_name_ext[0], $article_data[2]) == 0) {echo "1";die;}else{ echo "2";die;
+    $response[] = 'UID not included in filename. Example of a valid filename: "2986-1527NG - Bitcoin strategies and tips for beginners.docx"';  //strpos($file_name_ext[0], $splitdata[0]) !== false
+  }*/
+  
+  if(strpos($file_name_ext[0], $article_data[2]) === false) {
+     $response[] = 'UID not included in filename. Example of a valid filename: "2986-1527NG - Bitcoin strategies and tips for beginners.docx"';
+   }else if(strpos($file_name_ext[0], $article_data[2]) == '0'){
+    //Do nothing
+   }
+ 
   return $response;
 }
+
 /*
  * To get the length of doc file 
  * @param string $File path File name
  * @return array
  */
-function avs_read_doc_length($file_path) {
-  $response = array( );
-  $response['characters_length'] = 'Length of characters: '.filesize($file_path); 
-  $response['filename_length'] = 'Length of File Name: '.strlen($file_name[0]);
-  return $response;
+function avs_read_doc_length($file_path, $article_data) {
+  if (filesize($file_path) <= $article_data[12]) {
+    return 'Expected article minimum length: '.$article_data[12].' Actual article length: '.filesize($file_path);
+  }
+  return false;
 }
+
+/**
+ * Add custom field to user edit profile
+ * @param $user
+ */
+function avs_user_profile_custom_fields($user){
+  $avs_googlesheet_id = (get_user_meta($user->ID, 'avs_googlesheet_id', true)) ? get_user_meta($user->ID, 'avs_googlesheet_id', true) : '';
+  ?>
+  <table class="form-table">
+    <tr>
+      <th><label for="contact">Google Sheet ID</label></th>
+      <td><input type="text" class="input-text form-control" name="avs_googlesheet_id" id="avs_googlesheet_id" value="<?php echo $avs_googlesheet_id; ?>" /></td>
+    </tr>
+  </table>
+  <?php
+}
+add_action( 'show_user_profile', 'avs_user_profile_custom_fields' );
+add_action( 'edit_user_profile', 'avs_user_profile_custom_fields' );
+
+/**
+ * Save user custom fields
+ * @param User Id $user_id
+ */
+function avs_save_custom_user_profile_fields($user_id){
+  update_user_meta( $user_id, 'avs_googlesheet_id', $_POST['avs_googlesheet_id'] );
+}
+add_action( 'personal_options_update', 'avs_save_custom_user_profile_fields' );
+add_action( 'edit_user_profile_update', 'avs_save_custom_user_profile_fields' );
--- wp-content/themes/twentyseventeen-child/tmp-articles-submission-system.php
+++ wp-content/themes/twentyseventeen-child/tmp-articles-submission-system.php
@@ -3,6 +3,23 @@
  * Template Name: Articles submission system
  */
 wp_head();
+
+if( is_user_logged_in() ) {
+  $user = wp_get_current_user();
+
+  $roles = (array) $user->roles;
+  $disabled = '';
+  if( in_array('avs_writer', $roles) || in_array('shop_manager', $roles) || in_array('administrator', $roles) ){
+    // allow page render
+    $disabled = 'disabled="disabled"';
+  }else{
+    wp_redirect( wp_login_url( get_permalink() ) );
+    exit();
+  }
+} else {
+  wp_redirect( wp_login_url( get_permalink() ) );
+  exit();
+}
 ?>
 
 <div id="avs-listings" class="avs-listings-wrap">
@@ -11,16 +28,28 @@ wp_head();
       <p>
         <img width="50" height="50" src="<?php echo WP_THEME_URI . '/avs/assets/images/logo.png'; ?>" />
         <span><?php the_title();?></span>
-      </p>
-    </div>
+        <p>
+          <select onChange="top.location.href=this.options[this.selectedIndex].value;">
+            <?php
+            $users = get_users('role=avs_writer');
+            $writer = isset($_REQUEST['writer']) ? $_REQUEST['writer'] : '';
+            echo '<option value="'.get_the_permalink().'">--Select--</option>';
 
+            foreach ($users as $key => $value) {
+              echo '<option value="'.get_the_permalink().'?writer='.$value->ID.'" '.($writer == $value->ID ? ' selected="selected"' : "").'>'.avs_get_display_name($value->ID).'</option>';
+            }
+            ?>
+          </select>
+        </p>
+      </p>
+
+    </div>
     <div class="align-right">
-      <p>Loged In As: <?php echo avs_get_display_name(); ?></p>
+      <p>Logged In As: <?php echo avs_get_display_name(); ?></p>
     </div>
 
     <input id="avs-fileupload" type="file" name="files[]" >
   </header>
-
   <div class="avs-list-tbl-wrap">
     <table class="widefat fixed striped">
       <thead>
@@ -48,72 +77,86 @@ wp_head();
       </tbody>
     </table>
   </div>
-
-  <div class="avs-fileupload-status w-100">
-    <div id="files" class="files"></div>
-    <div id="progress" class="progress" >
-      <div class="progress-bar progress-bar-light progress-bar-striped "></div>
-    </div>
-    <div id="progress-error" style="color: red;"></div>
-  </div>
-
-  <?php $avs_current_user = avs_get_display_name(); ?>
-
+  
   <script type="text/html" id="tmpl-avs-listings">
-    <# _.each( data, function( item, index ){  #>
-      <# if(item[1] == '<?php echo $avs_current_user; ?>') { #>
-        <tr>
-          <td>&nbsp&nbsp{{{index+1}}}</td>
-          <td>{{{item[1]}}}</td>
-          <td>
-            <button type="button" class="btn btn-primary"  onclick="jQuery('#avs-fileupload').click();">Upload</button>
-          </td>
-          <td>{{{item[2]}}}</td>
-          <td>{{{item[4]}}}</td>
-          <td>{{{item[6]}}}</td>
-          <td>{{{item[7]}}}</td>
-          <td>{{{item[9]}}}</td>
-          <td>{{{item[10]}}}</td>
-          <td>{{{item[11]}}}</td>
-          <td>{{{item[12]}}}</td>
-          <td>{{{item[13]}}}</td>
-          <td>NA</td>
-        </tr>
-      <# } #>
+    <# _.each( data, function( item, index ){ #>
+      <tr class="new">
+        <td>&nbsp&nbsp{{{index+1}}}</td>
+        <td>{{{item[0]}}}</td>
+        <td>
+          <button type="button" class="uploadbtn"  data-index="{{{index}}}">Upload</button>
+        </td>
+        <td>{{{item[2]}}}</td>
+        <td>{{{item[4]}}}</td>
+        <td>{{{item[6]}}}</td>
+        <td>{{{item[7]}}}</td>
+        <td>{{{item[9]}}}</td>
+        <td>{{{item[10]}}}</td>
+        <td>{{{item[11]}}}</td>
+        <td>{{{item[12]}}}</td>
+        <td>{{{item[13]}}}</td>
+        <td>NA</td>
+      </tr>
     <# }) #>
   </script>
 
   <?php $data = avs_get_google_sheet_data();?>
 
-	<script type="text/javascript">
-		(function ($) {
-		  $(document).ready(function () {
-		    var tmpl_avs_listings = wp.template('avs-listings');
+  <script type="text/javascript">
+    (function ($) {
+      $(document).ready(function () {
+        var tmpl_avs_listings = wp.template('avs-listings');
+        var listings = JSON.parse(<?php echo json_encode($data['listings']); ?>);
+        console.log(listings);
+        for (i = 0; i < listings.length; i++) {
 
-		    // Quick search in large JSON data. Reference link - http://fusejs.io
-		    var options = { keys: ['6'] };
-		    var fuse = new Fuse(JSON.parse(<?php echo json_encode($data['listings']); ?>), options);
-		    var filterd_data = fuse.search('Pending, Validated');
+          if( listings[i][6] == 'Pending' || listings[i][6] == 'Validation Problems' ) { 
+            //Do Nothing
+          }else{
 
-        $('#avs-listings table tbody').html(tmpl_avs_listings(filterd_data));
+            listings.splice(i);
 
+          }
+        }
+
+        $('#avs-listings table tbody').html(tmpl_avs_listings(listings));
+
+        /**
+         * Handle upload button click
+         */
+        var row_data = [];
+        $(document).on('click', '.uploadbtn', function(){
+          var data_index = $(this).data('index');
+          row_data = listings[data_index];
+          $('#avs-fileupload').trigger('click');
+        });
+
+        /**
+         * File uploader
+         */
         $('#avs-fileupload').fileupload({
-          url: '<?php echo WP_SITE_URL; ?>/wp-content/uploads/articles-docs/',
+          url: '<?php echo WP_SITE_URL; ?>/wp-content/uploads/server/',
           dataType: 'json',
           add: function (e, data) { // https://goo.gl/yKUxaw
             var ext = data.originalFiles[0]['name'].split('.').pop();
+
             if (ext == 'docx' || ext == 'doc') {
-              $('#progress-error').html('');
               data.submit();
             } else {
-              $('#files').html(''); $('#progress .progress-bar').css('width', '0px');
-              $('#progress-error').html('*Only doc or docx files are allowed');
+              $('.progress').css('display', 'none');
+              $('.bPopup-body').html('<p>Only doc or docx files are allowed</p>');
+
+              $('.bPopup-dialog').bPopup({
+                closeClass:'bPopup-close',
+                follow: [false, false],
+                transition: 'slideDown'
+              });
             }
           },
           done: function (e, data) {
             $.each(data.result.files, function (index, file) {
               $('#files').html('<p>' + file.name + '</p>');
-
+              console.log(data.result.files);
               $.ajax({
                 url: '<?php echo admin_url('admin-ajax.php'); ?>',
                 type: 'POST',
@@ -121,13 +164,43 @@ wp_head();
                 data: {
                   action: 'avs_process_document',
                   file_name: file.name,
+                  article_data: JSON.stringify(row_data)
                 },
                 success : function( response ) {
-                  $.each(JSON.parse(response), function (key, val) {
-                    $('.avs-fileupload-status').append('<p>'+val+'</p>');
+                  var doc_upload_status  = '';
+                  // alert(response);
+                   // console.log(response);
+                  if (response == 'null') {
+                    doc_upload_status = '<p>Article document uploaded successfully</p>';
+                  }else{
+                    $.each(JSON.parse(response), function (key, val) {
+                      doc_upload_status = doc_upload_status + '<p>'+val+'</p>';
+                    });
+                  }
+
+                  $('.bPopup-body').html(doc_upload_status);
+
+                  $('.bPopup-dialog').bPopup({
+                    closeClass:'bPopup-close',
+                    follow: [false, false],
+                    transition: 'slideDown',
+                   onOpen: function() {
+                        if (response == 'null' && response) {
+                        $('tbody tr:first-child').slideUp('slow');
+                        $('#bPopup-message').text('Document uploaded');
+
+                      }
+                    },
+                    onClose: function() {
+                      if (response == 'null' && response) {
+                        $('tbody tr:first-child').slideUp('slow');
+                        $('#bPopup-message').text('Document uploaded');
+
+                      }
+                    }
                   });
                 }
-              });
+              });   
             });
           },
           progressall: function (e, data) {
@@ -135,9 +208,30 @@ wp_head();
             $('#progress .progress-bar').css('width', progress + '%');
           }
         });
-		  });
-		}(jQuery));
-	</script>
+      });
+    }(jQuery));
+  </script>
 </div>
+
+<div class="avs-fileupload-status">
+  <div id="files" class="files"></div>
+  <div id="progress" class="progress" >
+    <div class="progress-bar progress-bar-light progress-bar-striped "></div>
+  </div>
+</div>
+
+<div class="bPopup-dialog">
+  <div class="bPopup-content">
+    <div class="bPopup-header">
+      <h2 id="bPopup-message">Error Report</h2><hr> 
+    </div>
+    <div class="bPopup-body"></div>
+    <div class="bPopup-footer">
+      <hr><button type="button" class="bPopup-close">Ok</button>
+    </div>
+  </div>
+</div>
+
+
 
 <?php wp_footer();?>
